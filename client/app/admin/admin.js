0b41679f6c7925a423144e1683bcc46bc0c65427
Edu <e.marin.izquierdo@gmail.com>
2015-10-24 20:41:24 +0200

    first commit

---
+++ client/app/admin/admin.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('bablelandApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/admin', {
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
